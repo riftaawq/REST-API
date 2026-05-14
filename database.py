d6d911bb85bb5c85885091df74e7aa33d238d0ce
Roman_Orihivskij <@rorihivskij>
2026-05-14 18:53:32 +0300

    Fix

--- database.py
+++ database.py
@@ -1,8 +1,10 @@
 import motor.motor_asyncio
 import os
+import certifi
 
 MONGO_URL = os.getenv("MONGO_URL", "mongodb://mongo_admin:password@localhost:27017")
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
+
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL, tlsCAFile=certifi.where())
 db = client.library
 
 async def get_db():
